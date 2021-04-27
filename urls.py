6e20fffb92f95ab05365622ec1ffbc9e010abea6
Tanujaanand196 <tanujaanand196@gmail.com>
2021-04-28 02:32:12 +0530

    vqwghvd
     hjahg gc ac

--- urls.py
+++ urls.py
@@ -1,7 +1,16 @@
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from app import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls'))
+    path('', views.home),
+    path('product-detail/', views.product_detail, name='product-detail'),
+    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('buy/', views.buy_now, name='buy-now'),
+    path('profile/', views.profile, name='profile'),
+    path('address/', views.address, name='address'),
+    path('orders/', views.orders, name='orders'),
+    path('changepassword/', views.change_password, name='changepassword'),
+    path('mobile/', views.mobile, name='mobile'),
+    path('login/', views.login, name='login'),
+    path('registration/', views.customerregistration, name='customerregistration'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
